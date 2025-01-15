5080f3eb049b74c91e4434db110b5d039fa4f7e7
louisschraeder <louis.schraeder@hs-osnabrueck.de>
2025-01-15 15:21:59 +0900

    current version

--- src/ssm-zenoh.h
+++ src/ssm-zenoh.h
@@ -5,13 +5,20 @@
 #include "libssm.h"
 #include "ssm-time.h"
 
+#include "../external/zenoh-c/include/zenoh.h"
+
 /* ---- typedefs ---- */
+typedef struct {
+    z_owned_session_t session;
+} zenoh_context;
+
 // Structure for semaphore monitoring
 typedef struct {
     int suid;                       // shm id
     char name[SSM_SNAME_MAX];       // shm name
-    void (*callback)(int suid);   // Callback function to handle signals
+    void (*callback)(zenoh_context* z_context, int suid, char* name, int tid, ssm_header* shm_p);     // Callback function to handle signals
     volatile int* active;           // Flag to indicate if the thread is active
+    zenoh_context* z_context;       // Pointer to the Zenoh context
 } semaphore_arg;
 
 /* ---- function prototypes ---- */
