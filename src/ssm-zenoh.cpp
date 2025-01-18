349ddd36e2e907767e3155baa62d631f9ef705c1
louisschraeder <louis.schraeder@hs-osnabrueck.de>
2025-01-18 12:28:22 +0900

    add serialize/deserialize for the data

--- src/ssm-zenoh.cpp
+++ src/ssm-zenoh.cpp
@@ -238,8 +238,8 @@ void semaphore_callback(zenoh_context* z_context, shm_zenoh_info* shm_info) {
 
     // Create a Zenoh payload from the current shared memory content
     z_owned_bytes_t payload;
-    uint64_t* input_u64 = (uint64_t*) data;
-    if (ze_serialize_uint64(&payload, *input_u64) < 0) {
+    uint64_t input_u64 = (uint64_t) data;
+    if (ze_serialize_uint64(&payload, input_u64) < 0) {
         printf("Failed to create Zenoh payload from shared memory content.\n");
         free(data);
         free(ytime);
@@ -452,6 +452,7 @@ void data_handler(z_loaned_sample_t* sample, void* arg) {
     uint64_t output_u64 = 0;
     if (ze_deserialize_uint64(z_sample_payload(sample), &output_u64) < 0) {
         printf("Failed to deserialize payload\n");
+        return;
     }
 
     // printf("sub data size: %lu\n", sizeof((char*)output_u64));
@@ -462,6 +463,7 @@ void data_handler(z_loaned_sample_t* sample, void* arg) {
     SSM_tid tid = writeSSM( slist->ssmId, (char*)output_u64, time );
      if (tid < 0) {
          printf("Failed to write to shared memory\n");
+         return;
      }
 
     // printf(">> [Subscriber] Received %s ('%.*s': '%.*s')", kind_to_str(z_sample_kind(sample)),
